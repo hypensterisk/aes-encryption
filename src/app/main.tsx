e511827ea8cc99c590b50f8d0b5632238430990e
hypensterisk <roykhanalaushaf0@gmail.com>
2022-12-04 21:55:46 +0700

    update main.tsx

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -5,7 +5,7 @@ import {Container} from 'react-bootstrap'
 
 export default function Main({children}: {children?: ReactNode}) {
   return (
-    <main className='flex-shrink-0'>
+    <main className='flex-shrink-0 my-2'>
       <Container fluid>{children}</Container>
     </main>
   )
