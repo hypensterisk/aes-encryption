70a6ee9d4807b376aedca4d2b7e431718cbddb03
hypensterisk <roykhanalaushaf0@gmail.com>
2022-12-04 22:08:22 +0700

    feat: create about.tsx

---
+++ src/routes/about.tsx
@@ -0,0 +1,16 @@
+/** @format */
+
+import {Card} from 'react-bootstrap'
+
+export default function About() {
+  return (
+    <Card bg='primary' body border='light' text='light'>
+      <Card.Title>About</Card.Title>
+      <Card.Text>
+        "AES Encryption" Is A Web Application For Encrypting And Decrypting Data
+        With Advanced Encryption Standards, This Project Is For Study Purposes
+        Only And Not Intended For Serious Use.
+      </Card.Text>
+    </Card>
+  )
+}
