f4e44cd9f5c6d9cbe142d8631e2deab3b6fd94a1
hypensterisk <roykhanalaushaf0@gmail.com>
2022-12-04 18:13:43 +0700

    feat: create src/app/index.tsx

---
+++ src/app/index.tsx
@@ -0,0 +1,15 @@
+/** @format */
+
+import Footer from './footer'
+import Header from './header'
+import Main from './main'
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Main></Main>
+      <Footer />
+    </>
+  )
+}
